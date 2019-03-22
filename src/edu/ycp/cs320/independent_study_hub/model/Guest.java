2bcd16ddf20d8db81dacc74086f8f71b8292cda7
luc <lgartrell@ycp.edu>
2019-03-22 12:32:32 -0400

    created the abstract user class, and the following classes that inherit from it

---
+++ src/edu/ycp/cs320/independent_study_hub/model/Guest.java
@@ -0,0 +1,48 @@
+package edu.ycp.cs320.independent_study_hub.model;
+
+public class Guest extends User {
+	private String password, name, email;
+	// type is set to 0 because this is a guest account
+	private int type = 0;
+	
+	/**
+	 * constructor for the first time creation of a guest type of user **NOTE: type is not allowed to be changed,
+	 * will always be 0 for a guest type user
+	 * @param name = the name of the account being created
+	 * @param password = the password of the account being created
+	 * @param email = the email of the account being created
+	 */
+	public Guest(String name, String password, String email) {
+		this.name = name;
+		this.password = password;
+		this.email = email;
+	}
+	
+	// methods below are from the abstract user class, documentation is there
+	
+	@Override
+	String get_password() {
+		return password;
+	}
+
+	@Override
+	void change_password(String pass) {
+		password = pass;
+		
+	}
+
+	@Override
+	String get_name() {
+		return name;
+	}
+	
+	@Override
+	String get_email() {
+		return email;
+	}
+
+	@Override
+	int get_type() {
+		return type;
+	}
+}
