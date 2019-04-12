080092771e3e2661f4a41ddc5f733b6080004140
luc <lgartrell@ycp.edu>
2019-04-12 09:38:45 -0400

    get chemical get all sql THINGY working

---
+++ junit/edu/ycp/cs320/independent_study_hub/controller/SelectAllChemicalsControllerTest.java
@@ -0,0 +1,33 @@
+package edu.ycp.cs320.independent_study_hub.controller;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.ycp.cs320.independent_study_hub.model.ChemicalInventory;
+import edu.ycp.cs320.independent_study_hub.model.Student;
+
+public class SelectAllChemicalsControllerTest {
+	private SelectAllChemicalsController controller;
+	private ArrayList<ChemicalInventory> chem_list = new ArrayList<ChemicalInventory>();
+
+	@Before
+	public void setUp() throws Exception {
+		controller = new SelectAllChemicalsController();
+	}
+
+	@Test
+	public void test_sql_query_select_all() {
+		chem_list = controller.get_all_chemicals();
+		System.out.println("Printing....\n ");
+		System.out.println("Chemicals:");
+		for (ChemicalInventory c : chem_list) {
+			System.out.println(c.getChemicalID() + "| " + c.getChemical() + ", " + c.getUseOfChemical() + ", " + c.getDom());
+		}
+		System.out.println("\nPrint Complete");
+	}
+
+}
