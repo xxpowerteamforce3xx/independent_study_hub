d0210870154123ef2c0d201a7894f71c4fa6d75e
luc <lgartrell@ycp.edu>
2019-04-16 11:28:18 -0400

    made get one faculty db method, controller, and test

---
+++ junit/edu/ycp/cs320/independent_study_hub/controller/SelectOneFacultyControllerTest.java
@@ -0,0 +1,44 @@
+package edu.ycp.cs320.independent_study_hub.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.ycp.cs320.independent_study_hub.model.Faculty;
+import edu.ycp.cs320.independent_study_hub.model.Student;
+
+public class SelectOneFacultyControllerTest {
+	private SelectOneFacultyController2 controller;
+	private Faculty f;
+	private Faculty f1;
+	private Faculty f2;
+
+	@Before
+	public void setUp() throws Exception {
+		controller = new SelectOneFacultyController2();
+		f = new Faculty();
+		f1 = new Faculty();
+		f2 = new Faculty();
+	}
+
+	@Test
+	public void test_sql_query_select_all() {
+		f = controller.get_faculty("Dr.Steel");
+		System.out.println("Printing....\n ");
+		System.out.println("Faculty:");
+		System.out.println(f.getID() + "| " + f.get_name() + ", " + f.get_password() + ", " + f.get_email());
+		System.out.println("\nPrint Complete");
+		
+		f1 = controller.get_faculty("Dr.Foy");
+		System.out.println("Printing....\n ");
+		System.out.println("Student:");
+		System.out.println(f1.getID() + "| " + f1.get_name() + ", " + f1.get_password() + ", " + f1.get_email());
+		System.out.println("\nPrint Complete");
+		
+		f2 = controller.get_faculty("Dr.Fautch");
+		System.out.println("Printing....\n ");
+		System.out.println("Faculty:");
+		System.out.println(f2.getID() + "| " + f2.get_name() + ", " + f2.get_password() + ", " + f2.get_email());
+		System.out.println("\nPrint Complete");
+	}
+
+}
---
+++ src/edu/ycp/cs320/independent_study_hub/controller/SelectOneFacultyController2.java
@@ -0,0 +1,38 @@
+package edu.ycp.cs320.independent_study_hub.controller;
+
+import edu.ycp.cs320.independent_study_hub.persist.IDatabase;
+
+import java.util.ArrayList;
+
+import edu.ycp.cs320.independent_study_hub.model.ChemicalInventory;
+import edu.ycp.cs320.independent_study_hub.model.Faculty;
+import edu.ycp.cs320.independent_study_hub.model.Student;
+import edu.ycp.cs320.independent_study_hub.persist.DatabaseProvider;
+import edu.ycp.cs320.independent_study_hub.persist.DerbyDatabase;
+import edu.ycp.cs320.independent_study_hub.persist.FakeDatabase;
+public class SelectOneFacultyController2 {
+	private IDatabase db = null;
+	
+	/**
+	 * creates the db instance, for now setting it to only the fake db
+	 * since we dont hvae a real one
+	 * i spelled have wrong but im leaving it
+	 * sue me
+	 */
+	public SelectOneFacultyController2() {
+		// this will change to new DerbyDatabase when we get that goin
+		DatabaseProvider.setInstance(new DerbyDatabase()); 
+		db = DatabaseProvider.getInstance();
+	}
+	
+	/**
+	 * selects on student based on the name
+	 * @acc_name is the name of the acc we are looking for
+	 * @return returns a single student object
+	 */
+	public Faculty get_faculty(String acc_name) {
+		Faculty f = new Faculty();
+		f = db.get_faculty(acc_name);
+		return f;	
+	}
+}
